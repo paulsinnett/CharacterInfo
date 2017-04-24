254fe526497db59ddea45fbe465575a6494b9518
Paul Sinnett <paul.sinnett@gmail.com>
2017-04-24 19:14:25 +0100

    Test character info

---
+++ Assets/GetCharacterInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetCharacterInfo : MonoBehaviour
+{
+    public Text text;
+
+    void Start()
+    {
+        Invoke("DebugCharacterInfo", 1);
+    }
+
+    void DebugCharacterInfo()
+    {
+        CharacterInfo characterInfo;
+        text.font.GetCharacterInfo(text.text[0], out characterInfo, text.font.fontSize, text.fontStyle);
+        Debug.LogFormat("Character '{0}' width = {1} height = {2} advance = {3}",
+            text.text[0],
+            characterInfo.glyphWidth,
+            characterInfo.glyphHeight,
+            characterInfo.advance);
+    }
+
+    void Update()
+    {
+
+    }
+}
